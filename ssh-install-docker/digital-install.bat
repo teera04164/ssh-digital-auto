348e4a1faf8d5cc4d0512989d7614a756ee6058b
Teerapong <44111604+teera04164@users.noreply.github.com>
2021-08-15 16:47:59 +0700

    Update digital-install.bat

--- ssh-install-docker/digital-install.bat
+++ ssh-install-docker/digital-install.bat
@@ -1,5 +1,5 @@
 for /f "delims== tokens=1,2" %%G in (config.txt) do set %%G=%%H
 
-..\bin\plink.exe -ssh root@%IP_ADDRESS% -pw %PASSWORD%  -batch -m %cd%\deploy.sh
+..\bin\plink.exe -ssh root@%IP_ADDRESS% -pw %PASSWORD%  -batch -m %cd%\script.sh
 
 pause;
